57cb244fff018091e954f7ea45f3b7d2b0f2b3ca
jlmcgehee21 <jlmcgehee21@gmail.com>
2016-10-06 17:13:49 -0400

    use preserve=links with copy, change env vars to reference cuda rather than cuda-8.0

--- templates/cuda_env.sh
+++ templates/cuda_env.sh
@@ -1,3 +1,3 @@
-CUDA_HOME=/usr/local/cuda-8.0
-PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
-LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+CUDA_HOME=/usr/local/cuda
+PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
+LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
