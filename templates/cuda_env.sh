63c86460eac84c243a44c3054ac0d8e6a5b3fa76
jlmcgehee21 <jlmcgehee21@gmail.com>
2016-10-07 14:44:06 -0400

    make cuda_env template export

--- templates/cuda_env.sh
+++ templates/cuda_env.sh
@@ -1,3 +1,3 @@
-CUDA_HOME=/usr/local/cuda
-PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
-LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export CUDA_HOME=/usr/local/cuda
+export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
